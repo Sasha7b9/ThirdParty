790c3240cb234c5c21c1ef77b08d864ae4890ff6
Sasha7b9 <Sasha7b9@tut.by>
2019-12-22 15:30:02 +0300

    Теперь рантайм компилиуется статически в dll

--- scripts/wxWidgets/make_build_release.bat
+++ scripts/wxWidgets/make_build_release.bat
@@ -1,5 +1,5 @@
 cd ../../wxWidgets
 rmdir generated /s /q
 cd ../scripts/wxWidgets
-cmake.exe ..\..\wxWidgets\CMakeLists.txt -B..\..\wxWidgets\generated -DCMAKE_SYSTEM_VERSION=10.0.17763.0 -G "Visual Studio 16 2019" -A Win32 -DwxBUILD_SHARED=OFF -DwxBUILD_USE_STATIC_RUNTIME=ON
+cmake.exe ..\..\wxWidgets\CMakeLists.txt -B..\..\wxWidgets\generated -DCMAKE_SYSTEM_VERSION=10.0.17763.0 -G "Visual Studio 16 2019" -A Win32 -DwxBUILD_SHARED=ON -DwxBUILD_USE_STATIC_RUNTIME=ON
 call msbuild.bat
